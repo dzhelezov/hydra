ae835a44757e6d12f6652f10d353707e547891ff
Dmit <dzhelezov@gmail.com>
2021-05-25 17:20:10 +0300

    fix(hydra-cli): remove timestamp from the scaffold sources
    affects: @dzlzv/hydra-cli

--- packages/hydra-cli/src/templates/scaffold/mappings/index.ts
+++ packages/hydra-cli/src/templates/scaffold/mappings/index.ts
@@ -1,5 +1,1 @@
-// Export here all the event handler functions
-// so that the indexer picks them up
-//export { balancesTransfer as balances_Transfer } from './transfer'
-export { balancesTransfer, timestampCall } from './mappings'
-
+export { balancesTransfer } from './mappings'
--- packages/hydra-cli/src/templates/scaffold/mappings/mappings.ts
+++ packages/hydra-cli/src/templates/scaffold/mappings/mappings.ts
@@ -1,44 +1,25 @@
-import { Transfer, BlockTimestamp } from '../generated/graphql-server/model'
-
-// run 'NODE_URL=<RPC_ENDPOINT> EVENTS=<comma separated list of events> yarn codegen:mappings-types'
-// to genenerate typescript classes for events, such as Balances.TransferEvent
-import { Balances, Timestamp } from './generated/types'
+import { Transfer } from '../generated/graphql-server/model'
+import { Balances } from './generated/types'
 import BN from 'bn.js'
-import {
-  ExtrinsicContext,
-  EventContext,
-  StoreContext,
-} from '@dzlzv/hydra-common'
+import { EventContext, StoreContext } from '@dzlzv/hydra-common'
 
 export async function balancesTransfer({
   store,
   event,
-  block,
+  block: { height, timestamp },
   extrinsic,
 }: EventContext & StoreContext) {
   const transfer = new Transfer()
   const [from, to, value] = new Balances.TransferEvent(event).params
-  transfer.from = Buffer.from(from.toHex())
-  transfer.to = Buffer.from(to.toHex())
+  transfer.from = from.toHuman()
+  transfer.to = to.toHuman()
   transfer.value = value.toBn()
   transfer.tip = extrinsic ? new BN(extrinsic.tip.toString(10)) : new BN(0)
-  transfer.insertedAt = new Date(block.timestamp)
+  transfer.insertedAt = new Date(timestamp)
 
-  transfer.block = block.height
+  transfer.block = height
   transfer.comment = `Transferred ${transfer.value} from ${transfer.from} to ${transfer.to}`
-  transfer.timestamp = new BN(block.timestamp)
+  transfer.timestamp = new BN(timestamp)
   console.log(`Saving transfer: ${JSON.stringify(transfer, null, 2)}`)
   await store.save<Transfer>(transfer)
-}
-
-export async function timestampCall({
-  store,
-  event,
-  block,
-}: ExtrinsicContext & StoreContext) {
-  const call = new Timestamp.SetCall(event)
-  const blockT = new BlockTimestamp()
-  blockT.timestamp = call.args.now.toBn()
-  blockT.blockNumber = block.height
-  await store.save<BlockTimestamp>(blockT)
 }
